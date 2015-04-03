b7afc88fd87152f332439f528ba56ea4d103a352
Logan Hampton <losesmc@gmail.com>
2015-04-03 07:14:04 -0700

    Added the actual working code

--- fixcomments.js
+++ fixcomments.js
@@ -4,13 +4,37 @@
 app.addToolButton({
 	cName: "Fix Comments",
 	cExec: "fixComments()",
-	cTooltext: "Fix Comments",
+	cTooltext: "GIM Fix Comments",
 	cEnable: true,
-	cLabel: "Fix Comments"
+	cLabel: "GIM Fix Comments"
 });
 
 
 var fixComments = app.trustedFunction(
     function() {
-    	app.alert("Button works!")
-    });
+
+
+    	var annots, saveText;
+    	//Get annotations
+    	//console.show();
+    	for (var pg = 0; pg < this.numPages; pg++) {
+	    	annots = this.getAnnots({
+	    		nPage: pg
+	    	});
+	    	
+	    	if (annots === null) {
+	    		continue;
+	    	}
+
+	    	//Loop through annotations, find Carets and re enter the text for them
+	    	//to fix formatting problems
+	    	for (var i = 0; i < annots.length; i++) {
+	    		if (annots[i].type === "Caret") {
+	    			console.println("Caret contents are: " + annots[i].contents);
+	    			saveText = annots[i].contents;
+	    			annots[i].contents = saveText;
+	    		}
+	    	}
+    	}
+    	app.alert("Comments Fixed!")
+ 	});
