d4ce69f5b947f90e9b3e35134f8a895215a2635b
Logan Hampton <losesmc@gmail.com>
2016-09-12 08:41:43 -0700

    Update for now fixing Text and Highlight comments

--- fixcomments.js
+++ fixcomments.js
@@ -1,5 +1,5 @@
 //Fix Comments
-//2015 - Logan Hampton
+//2015-2016 - Logan Hampton
 
 app.addToolButton({
 	cName: "Fix Comments",
@@ -26,11 +26,12 @@ var fixComments = app.trustedFunction(
 	    		continue;
 	    	}
 
-	    	//Loop through annotations, find Carets and re enter the text for them
-	    	//to fix formatting problems
+	    	//Loop through annotations, finding Carets, Text ("Popups"), and Highlights
+            //to save their text and re-enter it to fix large sizes
 	    	for (var i = 0; i < annots.length; i++) {
-	    		if (annots[i].type === "Caret") {
-	    			console.println("Caret contents are: " + annots[i].contents);
+	    		if (annots[i].type === "Caret" || annots[i].type === "Text" ||
+                    annots[i].type === "Highlight") {
+	    			console.println(annots[i].type + " contents are: " + annots[i].contents);
 	    			saveText = annots[i].contents;
 	    			annots[i].contents = saveText;
 	    		}
